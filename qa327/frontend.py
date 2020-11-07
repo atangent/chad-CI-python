e0570aab452383993254a610ed6a128828865203
Seth Damiani <sethdamiani@gmail.com>
2020-11-07 14:39:24 -0500

    Fix authentication edge case
    Fixes an error that occured when logged_in was still in the session cookie but the user couldn't be found.

--- qa327/frontend.py
+++ qa327/frontend.py
@@ -109,6 +109,8 @@ def authenticate(inner_function):
                 # if the user exists, call the inner_function
                 # with user as parameter
                 return inner_function(user)
+            else:
+                return redirect('/login')
         else:
             # else, redirect to the login page
             return redirect('/login')
