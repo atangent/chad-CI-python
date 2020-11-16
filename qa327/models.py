c68a78b9c311e2031b8030e5f44c97e7448581eb
atangent <amy.tang@queensu.ca>
2020-11-16 15:35:34 -0500

    R5 - create ticket model

--- qa327/models.py
+++ qa327/models.py
@@ -24,6 +24,21 @@ class User(db.Model):
     balance = db.Column(db.Numeric(10,2), default=0)
 
 
+class Ticket(db.Model):
+    """
+    A ticket model which defines a ticket
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    holder = db.Column(db.String(100), unique=True)
+    date = db.Column(db.Integer(20300000))
+    name = db.Column(db.String(1000))
+    price = db.Column(db.Numeric(10,2), default=0)
+
+    tickets = []
+    def __init__(self):
+        self.tickets.append(self)
+        
+
 # it creates all the SQL tables if they do not exist
 with app.app_context():
     db.create_all()
